2696d9fa93656286e0cb933bffb9213863269325
pokulan <wojtekzom@gmail.com>
2016-12-06 11:44:40 +0100

    Sprzedaj Się

---
+++ app.js
@@ -0,0 +1,26 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/*
+app.get('/', function (req, res) {
+  res.send('Witaj na stronie NodeJS Express!');
+}); */
+
+app.use('/', express.static('home_page'));
+
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
