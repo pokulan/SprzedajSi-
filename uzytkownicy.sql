35de58988fe519568cbab12638cde18aa1ca9a4e
pokulan <wojtekzom@gmail.com>
2016-12-19 07:46:12 +0100

    t

--- uzytkownicy.sql
+++ uzytkownicy.sql
@@ -3,14 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 192.168.30.23
--- Generation Time: Dec 12, 2016 at 08:40 AM
+-- Generation Time: Dec 14, 2016 at 08:25 AM
 -- Server version: 5.5.52-0+deb8u1
--- PHP Version: 5.6.27-0+deb8u1
+-- PHP Version: 7.0.14-1+0~20161212122340.19+jessie~1.gbp8dc455
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-USE OGLOSZENIOWA;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -36,14 +36,14 @@ CREATE TABLE `uzytkownicy` (
   `datarej` date NOT NULL,
   `punkty` int(11) NOT NULL,
   `ogloszenia` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `uzytkownicy`
 --
 
-INSERT INTO `uzytkownicy` (`id`, `mail`, `imie`, `nazwisko`, `nick`, `haslo`, `datarej`, `punkty`, `ogloszenia`) VALUES
-(0, 'wojtekzom@gmail.com', 'Wojtas', 'Zom', 'pokulan', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2016-12-12', 5, 0);
+INSERT INTO `uzytkownicy` (`mail`, `imie`, `nazwisko`, `nick`, `haslo`, `datarej`, `punkty`, `ogloszenia`) VALUES
+('wojtekzom@gmail.com', 'Wojtas', 'Zom', 'pokulan', 'd8578edf8458ce06fbc5bb76a58c5ca4', '2016-12-12', 5, 0);
 
 --
 -- Indexes for dumped tables
@@ -52,8 +52,16 @@ INSERT INTO `uzytkownicy` (`id`, `mail`, `imie`, `nazwisko`, `nick`, `haslo`, `d
 --
 -- Indexes for table `uzytkownicy`
 --
-ALTER TABLE `uzytkownicy`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `kategoria`
+  ADD PRIMARY KEY (`id`),
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uzytkownicy`
+--
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
